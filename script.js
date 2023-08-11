c2246fb645312e71208569c26a0281b00b429adb
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-11 11:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
 	for(let i in sampleObject){
-		if(sampleObject[i] == key){
+		if(i == key){
 			return true;
 		}
 	}
